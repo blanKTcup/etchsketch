eab17e6f765c18496dbc3ca00cfa70992025c1c7
Kevin Tran <kthightran024@gmail.com>
2024-05-16 16:12:05 -0700

    added slider value functionality displaying the correct dimensions

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const gridContainerEl = document.querySelector('.grid-container');
+const dimensionsEl = document.querySelector('.dimensions');
+const sliderEL = document.querySelector('.slider');
+
+dimensionsEl.innerHTML = sliderEL.value + "x16"; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+sliderEL.oninput = function() {
+  dimensionsEl.innerHTML = this.value + "x" + this.value;
+}
+
 
 
 for (let i = 0; i <= sizeInputEl.value; i++) {
