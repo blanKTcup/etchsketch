7b1e20a25779d706cf96a95da85fc404ec6ae87b
Kevin Tran <kthightran024@gmail.com>
2024-05-15 21:10:05 -0700

    added slider html and css

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 const gridContainerEl = document.querySelector('.grid-container');
+
+
+for (let i = 0; i <= sizeInputEl.value; i++) {
+  const gridItem = document.createElement('div');
+  gridContainerEl.appendChild(gridItem);
+}
