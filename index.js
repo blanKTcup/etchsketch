98e9178b7709964627cf60d094b966d5307145ba
Kevin Tran <kthightran024@gmail.com>
2024-05-23 17:54:23 -0700

    added dragging and hover drawing functionality

--- index.js
+++ index.js
@@ -5,56 +5,57 @@ const sliderEL = document.querySelector('.slider');
 const initialSliderValue = sliderEL.value;
 
 function initialGrid() {
-  
-  dimensionsEl.innerHTML = initialSliderValue + "x16"; // Display the default slider value
-  gridSetup(initialSliderValue);
+	dimensionsEl.innerHTML = initialSliderValue + 'x16'; // Display the default slider value
+	gridSetup(initialSliderValue);
 }
-
-
 
 // Update the current slider value (each time you drag the slider handle)
-// sliderEL.oninput = (e) => updateDimensions(e.target.value)
+// sliderEL.oninput = (e) => updateDimensions(e.target.value) second option for getting value
 
-sliderEL.oninput = function() {
-  updateDimensions(this.value);
-}
+sliderEL.oninput = function () {
+	updateDimensions(this.value);
+};
 
 function updateDimensions(gridVal) {
-  updateDimensionsVal(gridVal);
-  clearGrid();
-  gridSetup(gridVal);
+	updateDimensionsVal(gridVal);
+	clearGrid();
+	gridSetup(gridVal);
 }
 
 function updateDimensionsVal(value) {
-  dimensionsEl.innerHTML = value + "x" + value;
+	dimensionsEl.innerHTML = value + 'x' + value;
 }
 
 function clearGrid() {
-  gridContainerEl.innerHTML = '';
+	gridContainerEl.innerHTML = '';
 }
+
+let mouseDown = false;
+document.body.onmousedown = () => {
+  mouseDown = true;
+  console.log('mousedown is true');
+};
+document.body.onmouseup = () => {
+  mouseDown = false;
+  console.log('mousedown is false');
+};
 
 function gridSetup(value) {
-  for (let i = 0; i <= (value * value) - 1; i++) {
-    const gridItem = document.createElement('div');
-    gridContainerEl.appendChild(gridItem);
-    gridItem.style.width = (500/value) + 'px';
-    gridItem.style.height = (500/value) + 'px';
-    gridItem.addEventListener('mouseover');
-    gridItem.addEventListener('mousedown', () => addColor(gridItem));
-    if ('mousedown' === true) {
-      addColor(gridItem);
-    }
-    // gridItem.addEventListener('mouseup', () => unDrag(gridItem));
-  }
+	for (let i = 0; i <= value * value - 1; i++) {
+		const gridItem = document.createElement('div');
+		gridItem.style.width = 500 / value + 'px';
+		gridItem.style.height = 500 / value + 'px';
+		gridItem.addEventListener('mousedown', (e) => addColor(gridItem, e));
+		gridItem.addEventListener('mouseover', (e) => addColor(gridItem, e));
+		gridContainerEl.appendChild(gridItem);
+	}
 }
 
-function addColor(gridItem) {
-  gridItem.classList.add('grid-shading');
+function addColor(gridItem, e) {
+	if (e.type = 'mouseover' && !mouseDown) return;
+	else {
+		gridItem.classList.add('grid-shading');
+	}
 }
-
-// function unDrag(gridItem) {
-//   gridItem.removeEventListener('mouseover');
-// }
 
 initialGrid();
-
